e1e9793f87d836500ea756b5954f11889f7af827
Shawn <Shawn-CMS@users.noreply.github.com>
2016-11-16 19:08:57 +0200

    Add files via upload

--- includes/connection.php
+++ includes/connection.php
@@ -1,16 +1,18 @@
 <?php
-	// database settings
-	$database = array(
-		'user' 		=> "root",
-		'pass' 		=> "",
-		'dbname'  	=> "s-cms"
-	);	
-	
-	try {
-		$SCMS_CONN = new PDO('mysql:host=localhost;dbname='. $database["dbname"] .'', $database["user"], $database["pass"]);
-	}
-	catch (PDOException $e) {
-		print "Error!: " . $e->getMessage() . "<br/>";
-		die();
+	function SCMSDB()
+	{
+		$dbhost = "localhost";
+		$dbuser = "root";
+		$dbpass = "";
+		$dbname = "s-cms";
+		try {
+			$dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass); 
+			$dbConnection->exec("set names utf8");
+			$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $dbConnection;
+		}
+		catch (PDOException $e) {
+			echo 'Connection failed: ' . $e->getMessage();
+		}
 	}
 ?>
